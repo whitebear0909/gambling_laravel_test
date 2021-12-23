49d424e6fa26f66eef46003ea1de7b8a3ca2700c
whitebear0909 <boris.whitebear0909@outlook.com>
2021-12-23 22:34:06 +0900

    remove unused code.

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-//Route::get('/{any}', 'App\Http\Controllers\HomeController::class@index')->where('any', '.*');
 Route::get('/{any}', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->where('any', '.*');
 
